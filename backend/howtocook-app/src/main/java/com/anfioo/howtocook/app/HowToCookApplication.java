ae3bf508aa00df66c4e4503145da658a5c757b56
soapsama7 <1692226484@qq.com>
2026-09-16 21:28:36 +0800

    step-0.1: Maven 多模块改造，Spring Boot 降级至 3.5.x

---
+++ backend/howtocook-app/src/main/java/com/anfioo/howtocook/app/HowToCookApplication.java
@@ -0,0 +1,13 @@
+package com.anfioo.howtocook.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowToCookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowToCookApplication.class, args);
+	}
+
+}
